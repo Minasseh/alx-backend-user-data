1b63d8efb7bccc0a30592cb04353f997b9ef9bbd
root <mnsalaro@yahoo.com>
2024-05-30 08:03:02 -0700

    Regexing

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+""" Logging Filter """
+
+import typing
+import re
+
+
+def filter_datum(fields: typing.List[str], redaction: str,
+                 message: str, separator: str) -> str:
+    """ A function called filter_datum that returns
+    the log message obfuscated """
+    return re.sub(separator.join(fields) + r'\b', redaction, message)
