6057cc8e89f18fc86035b37b710cde85073ee9d7
Minasseh <mnsalaro@yahoo.com>
2024-06-06 13:02:46 +0300

    Update

--- 0x02-Session_authentication/api/v1/views/users.py
+++ 0x02-Session_authentication/api/v1/views/users.py
@@ -25,13 +25,17 @@ def view_one_user(user_id: str = None) -> str:
       - User object JSON represented
       - 404 if the User ID doesn't exist
     """
-    if user_id == me and request.current_user is None:
+    if user_id is None:
         abort(404)
+    if user_id == "me":
+        me = request.current_user
+        if not me:
+            abort(404)
+        return jsonify(me.to_json())
     user = User.get(user_id)
     if user is None:
         abort(404)
-    if user_id == me and request.current_user is not None:
-        return jsonify(user.to_json())
+    return jsonify(user.to_json())
 
 
 @app_views.route('/users/<user_id>', methods=['DELETE'], strict_slashes=False)
